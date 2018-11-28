e284069dcc2325bdb4ea9c3dc3b0025da88f2c11
Piotr Nowak <p.nowak2@gmail.com>
2018-11-28 09:13:12 +0100

    add timewindow support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TimeWindow } from './coverage-chart/model/coverage.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'asm-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  timeWindow: TimeWindow;
+
+  constructor() {
+    this.timeWindow = {
+      from: moment('1/05/2017', 'DD-MM-YYYY').toDate(),
+      to: moment('3/08/2018', 'DD-MM-YYYY').toDate()
+    };
+  }
 }
--- src/app/coverage-chart/service/coverage.service.ts
+++ src/app/coverage-chart/service/coverage.service.ts
@@ -4,37 +4,37 @@ import { TimeWindow, AxisLabel } from '../model/coverage.model';
 
 @Injectable()
 export class CoverageService {
-  constructor() { }
 
-  createTimeWindow(timeWindow: TimeWindow = {}): TimeWindow {
-    return {};
+  getMonthsDifference(from: Date, to: Date) {
+    return moment(to).diff(from, 'months', false) + 1;
+  }
+
+  sanitizeTimeWindow(timeWindow: TimeWindow): TimeWindow {
+    const to = timeWindow.to || moment().toDate();
+    const from = timeWindow.from || moment(to)
+      .subtract(19, 'months')
+      .toDate();
+
+    return { from, to };
+  }
+
+  createArray(n: number) {
+    return Array.from(
+      Array(n).keys()
+    );
   }
 
   createAxisLabels(timeWindow: TimeWindow): Array<AxisLabel> {
-    const { from, to } = this.createTimeWindow(timeWindow);
+    const tw = this.sanitizeTimeWindow(timeWindow);
+    const monthsCount = this.getMonthsDifference(tw.from, tw.to);
+    const monthsToIterate = this.createArray(monthsCount);
 
-    return [
-      { position: 0, text: '1/18' },
-      { position: 5, text: '2/18' },
-      { position: 10, text: '3/18' },
-      { position: 15, text: '4/18' },
-      { position: 20, text: '5/18' },
-      { position: 25, text: '6/18' },
-      { position: 30, text: '7/18' },
-      { position: 35, text: '8/18' },
-      { position: 40, text: '9/18' },
-      { position: 45, text: '10/18' },
-      { position: 50, text: '11/18' },
-      { position: 55, text: '1/18' },
-      { position: 60, text: '2/18' },
-      { position: 65, text: '3/18' },
-      { position: 70, text: '4/18' },
-      { position: 75, text: '5/18' },
-      { position: 80, text: '6/18' },
-      { position: 85, text: '7/18' },
-      { position: 90, text: '8/18' },
-      { position: 95, text: '9/18' },
-      { position: 100, text: '10/18' },
-    ];
+    return monthsToIterate.map(monthNumber => {
+      const currentFrom = moment(tw.from).add(monthNumber, 'month');
+      const text = currentFrom.format('MM/YY');
+      const position = (monthNumber / (monthsCount - 1)) * 100;
+
+      return { position, text };
+    });
   }
 }
